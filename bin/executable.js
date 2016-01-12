676c0c817e28af5403b76e4b64864601ad1ba33c
fiatjaf@spooner <fiatjaf@gmail.com>
2016-01-12 13:36:35 -0500

    fail gracefully when Node is older than 4.0.0.
    fixes #2

--- bin/executable.js
+++ bin/executable.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
-'use strict'
+if (parseInt(process.versions.node.split('.')[0], 10) < 4) {
+  console.log("I'm sorry, this app requires a Node version equal or higher than 4.0.0.")
+  console.log('Yours is ' + process.versions.node + '.')
+  console.log('Please look at http://stackoverflow.com/questions/10075990/upgrading-node-js-to-latest-version/10076029#10076029 for a super easy guide on how to upgrade.')
+  process.exit()
+}
 
-const cli = require('..')
+var cli = require('..')
 
-setTimeout(() => {
+setTimeout(function () {
   if (cli.find('auth')) {
     cli.exec('auth')
   }
